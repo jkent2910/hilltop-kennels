96ad174560c694142f4d7f81513b19c5a88bdf16
Julie Kent <jkent2910@gmail.com>
2016-11-06 20:05:51 -0600

    wizzy wig

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,21 @@
 //= require plugins/align.min.js
 //= require plugins/font_family.min.js
 //= require plugins/font_size.min.js
+//= require plugins/colors.min.js
+//= require plugins/image.min.js
+//= require plugins/image_manager.min.js
+//= require plugins/inline_style.min.js
+//= require plugins/line_breaker.min.js
+//= require plugins/link.min.js
+//= require plugins/lists.min.js
+//= require plugins/paragraph_format.min.js
+//= require plugins/paragraph_style.min.js
+//= require plugins/quick_insert.min.js
+//= require plugins/quote.min.js
+//= require plugins/save.min.js
+//= require plugins/table.min.js
+//= require plugins/url.min.js
+//= require plugins/video.min.js
 //= require turbolinks
 //= require_tree .
 
